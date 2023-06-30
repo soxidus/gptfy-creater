70fe4c7592418cfef936b4a34038a5455cfce309
soxidus <s6chratt@uni-bonn.de>
2023-07-01 00:18:20 +0200

    setup structure and see where we're going

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import inquirer
+import dotenv
+dotenv.load_dotenv()
+
+from gpt_handler import *
+from spotify_handler import *
+
+
+
+
+
+token = os.environ.get("api-token")
+
+def main():
+    main_promt = [
+        inquirer.List('choices',
+                      message="Choose Mode",
+                      choices=['Simple (Bands)', 'Advanced (Mood, adjectives...)'],
+                      ),
+    ]
+
+    _ = inquirer.prompt(main_promt)
+
+    if _['choices'] == 'Simple (Bands)':
+        print("simple")
+
+    if _['choices'] == 'Advanced (Mood, adjectives...)':
+        print("advanced")
+
+
+if __name__ == '__main__':
+    main()
+
